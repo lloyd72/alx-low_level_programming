f47c200e8a771f0f90ca30cf18d89f5807dc8313
lloyd72 <lloydsintim128@gmail.com>
2023-02-16 07:32:49 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-<<<<<<< HEAD
  * main - A c program that print a line to the standard error
- * Return:1 (Success)
-=======
- * main - A program that print a line to the standard error
- * Return: 1 (Success)
->>>>>>> b47e604281cf321690a19dd3a68b6e3b908f8be7
+ * Return:1 (Success)7
  */
 int main(void)
 {
