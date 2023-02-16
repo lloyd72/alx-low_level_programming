b3ef9bf4350e0f4d0ff5ecdf387522c3b49373b1
lloyd72 <122834454+lloyd72@users.noreply.github.com>
2023-02-16 15:07:05 +0000

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 #include <unistd.h>
 /**
  * main - A program that print a line to the standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  */
 int main(void)
 {
-write(2,
+fprintf(
 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
