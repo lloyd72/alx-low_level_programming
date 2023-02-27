307403bb71a298ef90bf23ca4416b42f75959e5f
lloyd72 <lloydsintim128@gmail.com>
2023-02-27 07:57:04 -0800

    Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,23 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
+/**
+ * main - generate valid random passwords for thr crackme program
+ *
+ * Return: Alwyas 0.
+ */
 int main(void)
 {
-  int n;
-  int a[5];
-  int *p;
+	int sum = 2772;
+	int n;
 
-  a[2] = 1024;
-  p = &n;
-  /*
-   * write your line of code here...
-   * Remember:
-   * - you are not allowed to use a
-   * - you are not allowed to modify p
-   * - only one statement
-   * - you are not allowed to code anything else than this line of code
-   */
-  *(p + 5) = 98;
-  /* ...so that this prints 98\n */
-  printf("a[2] = %d\n", a[2]);
-  return (0);
+	srand(time(0));
+	while (sum)
+	{
+		n = rand() % 100;
+		if (n <= 94)
+			n += 32;
+		else
+			continue;
+		if (sum - n == 0)
+		{
+			sum -= n;
+			printf("%c", n);
+		}
+		else if (sum - n - 32 > 0)
+		{
+			sum -= n;
+			printf("%c", n);
+		}
+	}
+	return (0);
 }
