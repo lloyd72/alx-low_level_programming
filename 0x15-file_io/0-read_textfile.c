288ab21c516f420b5fb8c64ab48a30d93e203aca
lloyd72 <lloydsintim128@gmail.com>
2023-04-07 12:23:20 -0700

    C - File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads text file and prints it to POSIX stdout
+ * filename: poiinter to file
+ * @letters: size of filename
+ *
+ * Return: number of letter
+ * If function failks, return 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+	free(buf);
+
+	return (nwr);
+}
