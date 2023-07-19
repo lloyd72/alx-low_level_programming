b80df4f4cdfa824e8281c68af21a98dfacad548a
lloyd72 <lloydsintim128@gmail.com>
2023-07-19 15:22:20 -0700

    Function pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 /**
  * array_iterator - executes a function on each array element
- * @array: array of elements to execute 
+ * @array: array of elements to execute
  * @action: pointer to the function to be executed
  * @size: elements numb to print
  *
